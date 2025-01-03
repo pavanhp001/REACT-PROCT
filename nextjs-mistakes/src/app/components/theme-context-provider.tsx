f913428d9f2f0cf03d60f928db827856c9044b15
pavanhp001 <pavan.hp001@gmail.com>
2025-01-03 16:56:11 +0530

    5. using state management  ( context api, zustand, Redux) in server comp

--- nextjs-mistakes/src/app/components/theme-context-provider.tsx
+++ nextjs-mistakes/src/app/components/theme-context-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { createContext, useState } from 'react'
 import Product from './product';
-const ThemeContext = createContext("light");
+const ThemeContext = createContext("light");  // if we remove all useState logic also its client comp becouse here we are using createContext abjct
 
 export default function ThemeContextProvider({ children }) {
     const [theme, setTheme] = useState("light");
