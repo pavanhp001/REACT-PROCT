8ca5c6fb9ae01c51d6a910c2f772c506b1be5180
pavanhp001 <pavan.hp001@gmail.com>
2025-01-03 15:09:23 +0530

    2. Not refactoring for use client

--- nextjs-mistakes/src/app/page.tsx
+++ nextjs-mistakes/src/app/page.tsx
@@ -2,6 +2,7 @@
 //if we add "use client" to tla page becomes cleint comp means product also become client.
 import FavoriteBtn from "./components/favorite-btn";
 import Product from "./components/product";
+import UpvoteBtn from "./components/upvote-btn";
 
 export default function Home() {
   return (
@@ -11,6 +12,11 @@ export default function Home() {
         <h1>My store </h1>
         <Product/>
         <FavoriteBtn/>
+        {/* //if we want to add new button and use and client component for this entire component becouse for the only button
+        <button onClick={() => {}}>Update</button> */}
+        {/* Instead create new component and update it as client component */}
+
+        <UpvoteBtn/>
       </main>
     </div>
   );
